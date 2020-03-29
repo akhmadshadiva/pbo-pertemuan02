15b1f2663359307cd9c9499c24cd88fad664c09d
akhmadshadiva <akhmadshadiva12@gmail.com>
2020-03-29 17:37:43 +0800

    Memanggil method dari Class di package lain

--- koneksi/Database.java
+++ koneksi/Database.java
@@ -1,7 +1,7 @@
 package koneksi;
 
 public class Database{
- public static boid hubungkan(){
-  System.out.ptintln("Database Terhubung..");
+ public static void hubungkan(){
+  System.out.println("Database Terhubung..");
  }
 }
