16c77fafd01b6ea86148095754c8b242cfea42b3
akhmadshadiva <akhmadshadiva12@gmail.com>
2020-03-29 20:18:13 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,5 +7,8 @@ public class Main{
  public static void main(String [] args){
   System.out.println("Ini program main");
   Database.hubungkan();
+  Dosen.tampilanInfo();
+  Mahasiswa.tampilanInfo();
+  MataKuliah.tampilanInfo();
  }
 }
